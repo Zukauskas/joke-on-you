18f8b76d196a7463c22ed86d612e66083dc2f1ca
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-03 14:05:02 +0200

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-    return <div className='App'></div>;
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch('https://v2.jokeapi.dev/joke/Programming?amount=10')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {items.jokes.map((item) => {
+                    if (item.type === 'single') {
+                        return <li>{item.joke}</li>;
+                    } else {
+                        return (
+                            <li>
+                                {item.setup} {item.delivery}
+                            </li>
+                        );
+                    }
+                })}
+            </ul>
+        );
+    }
 }
 
 export default App;
